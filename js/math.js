163079fcc30520d1b324bb7b5786dfb8f27d87e5
Jonathan Fischer Friberg <odyssomay@gmail.com>
2011-10-28 13:13:13 +0200

    compiled with bare option

--- js/math.js
+++ js/math.js
@@ -1,59 +1,57 @@
-(function() {
-  var abs, acos, asin, atan, atan2, ceil, cos, e, exp, floor, log, max, min, pi, pow, random, round, sin, sqrt, tan;
-  abs = function(x) {
-    return Math.abs(x);
-  };
-  acos = function(x) {
-    return Math.acos(x);
-  };
-  asin = function(x) {
-    return Math.asin(x);
-  };
-  atan = function(x) {
-    return Math.atan(x);
-  };
-  atan2 = function(p, q) {
-    return Math.atan2(p, q);
-  };
-  ceil = function(x) {
-    return Math.ceil(x);
-  };
-  cos = function(x) {
-    return Math.cos(x);
-  };
-  exp = function(x) {
-    return Math.exp(x);
-  };
-  floor = function(x) {
-    return Math.floor(x);
-  };
-  log = function(x) {
-    return Math.log(x);
-  };
-  max = function() {
-    return Math.max.apply(null, arguments);
-  };
-  min = function() {
-    return Math.min.apply(null, arguments);
-  };
-  pow = function(a, b) {
-    return Math.pow(a, b);
-  };
-  random = function() {
-    return Math.random();
-  };
-  round = function(x) {
-    return Math.round(x);
-  };
-  sin = function(x) {
-    return Math.sin(x);
-  };
-  sqrt = function(x) {
-    return Math.sqrt(x);
-  };
-  tan = function(x) {
-    return Math.tan(x);
-  };
-  e = Math.E;
-  pi = Math.PI;
-}).call(this);
+var abs, acos, asin, atan, atan2, ceil, cos, e, exp, floor, log, max, min, pi, pow, random, round, sin, sqrt, tan;
+abs = function(x) {
+  return Math.abs(x);
+};
+acos = function(x) {
+  return Math.acos(x);
+};
+asin = function(x) {
+  return Math.asin(x);
+};
+atan = function(x) {
+  return Math.atan(x);
+};
+atan2 = function(p, q) {
+  return Math.atan2(p, q);
+};
+ceil = function(x) {
+  return Math.ceil(x);
+};
+cos = function(x) {
+  return Math.cos(x);
+};
+exp = function(x) {
+  return Math.exp(x);
+};
+floor = function(x) {
+  return Math.floor(x);
+};
+log = function(x) {
+  return Math.log(x);
+};
+max = function() {
+  return Math.max.apply(null, arguments);
+};
+min = function() {
+  return Math.min.apply(null, arguments);
+};
+pow = function(a, b) {
+  return Math.pow(a, b);
+};
+random = function() {
+  return Math.random();
+};
+round = function(x) {
+  return Math.round(x);
+};
+sin = function(x) {
+  return Math.sin(x);
+};
+sqrt = function(x) {
+  return Math.sqrt(x);
+};
+tan = function(x) {
+  return Math.tan(x);
+};
+e = Math.E;
+pi = Math.PI;
